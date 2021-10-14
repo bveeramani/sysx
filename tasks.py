d3b531b3ff040bd24d940ecbb07d2695b2714d79
Balaji Veeramani <bveeramani@berkeley.edu>
2021-10-13 21:55:27 -0700

    Update lint.yml

--- tasks.py
+++ tasks.py
@@ -3,15 +3,6 @@ from invoke import task  # pylint: disable=import-error
 
 
 @task
-def setup(c):
-    """Set up developer environment."""
-    url = "https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py"
-    c.run(f"curl -sSL {url} | python -")
-    c.run("export PATH=$PATH:$HOME/.poetry/bin")
-    c.run("poetry install")
-
-
-@task
 def pylint(c):
     """Lint code."""
     c.run("poetry run pylint sysx tests tasks.py")
